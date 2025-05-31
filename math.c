7bbeffbbc06c1b4818e31c4140c3c9a4835a6f07
mistbornnn <Mistborn682@gmail.com>
2025-05-31 14:44:08 -0700

    Add files

---
+++ math.c
@@ -0,0 +1,29 @@
+#include "math.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+float average(int *arr, int size) {
+    if (size <= 0) return 0.0f;
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return (float)sum / size;
+}
+
+int max(int *arr, int size) {
+    if (size <= 0) return 0;
+    int max_val = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+    return max_val;
+}
---
+++ math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+float average(int *arr, int size);
+int max(int *arr, int size);
+
+#endif
