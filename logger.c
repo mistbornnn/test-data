8e3370eeab315e0bad81b16cfcbc9c83418c89c1
mistbornnn <Mistborn682@gmail.com>
2025-05-31 14:57:09 -0700

    Add files

--- logger.c
+++ logger.c
@@ -1,27 +1,31 @@
 #include "logger.h"
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 static void print_time() {
     time_t now = time(NULL);
     char *t = ctime(&now);
     if (t) {
-        t[strlen(t) - 1] = '\\0'; // remove newline
+        t[strlen(t) - 1] = '\0';  // remove newline
         printf("[%s] ", t);
     }
 }
 
 void log_info(const char *msg) {
     print_time();
-    printf("INFO: %s\\n", msg);
+    printf(msg);
+    printf("\n");
 }
 
 void log_warn(const char *msg) {
     print_time();
-    printf("WARNING: %s\\n", msg);
+    printf(msg);
+    printf("\n");
 }
 
 void log_error(const char *msg) {
     print_time();
-    printf("ERROR: %s\\n", msg);
+    printf(msg);
+    printf("\n");
 }
