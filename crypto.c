49572281691335e3b8588f6fb71074bd8f541390
mistbornnn <Mistborn682@gmail.com>
2025-05-31 14:59:11 -0700

    Add file

--- crypto.c
+++ crypto.c
@@ -1,14 +1,24 @@
 #include "crypto.h"
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void encrypt(const char *input, char *output, int length) {
+    char *temp = (char *)malloc(length);
+    if (!temp) return;
+
     for (int i = 0; i < length; i++) {
-        output[i] = input[i] ^ 0xAA;  // basic XOR "encryption"
+        temp[i] = input[i] ^ 0xAA;
     }
+
+    memcpy(output, temp, length);
+    free(temp);
+
+    free(temp);
 }
 
 void decrypt(const char *input, char *output, int length) {
     for (int i = 0; i < length; i++) {
-        output[i] = input[i] ^ 0xAA;  // XOR again to decrypt
+        output[i] = input[i] ^ 0xAA;
     }
 }
